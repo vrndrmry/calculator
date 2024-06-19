c15a73ba398e0d00ee6585d86c75a3189cc7131b
vrndrmry <117343379+vrndrmry@users.noreply.github.com>
2024-06-19 23:19:35 +0530

    first commit

--- src/Components/SimpleCalculator/SimpleCalculator.js
+++ src/Components/SimpleCalculator/SimpleCalculator.js
@@ -1,30 +1,51 @@
-import React from "react";
-import './simpleCalculator.css'
+import React, { useState } from "react";
+import "./simpleCalculator.css";
 
 export default function SimpleCalculator() {
+  const [input, setInput] = useState("50++++");
+  const [result, setResult] = useState(0);
+
+  const handleInput = (input) => {
+    console.log(input);
+  };
+  const clearFunction = () => {
+    setInput(0);
+  };
+  const stepClearFunction=()=>{
+
+      setInput(input.slice(0, -1));
+    
+  }
+  const inverseCalculate=()=>{
+    setInput(1/input)
+  }
+  const squareCalculate=()=>{
+    setInput(input**2)
+  }
   return (
     <div className="standardCalculatorDiv">
-    
       <table>
-
         <tbody>
-        <tr>
-        <td colSpan={4}>0</td>
-        </tr>
+          <tr>
+            <td colSpan={4}>{input}</td>
+          </tr>
+          <tr>
+            <td colSpan={"4".toString()}>Result: {result}</td>
+          </tr>
           <tr>
             <td>%</td>
-            <td>CE</td>
-            <td>C</td>
+            <td onClick={stepClearFunction}>CE</td>
+            <td onClick={clearFunction}>C</td>
             <td>&#11013;</td>
           </tr>
           <tr>
-            <td>1/x</td>
-            <td>x&sup2;</td>
+            <td onClick={inverseCalculate}>1/x</td>
+            <td onClick={squareCalculate}>x&sup2;</td>
             <td>&radic;x</td>
             <td>&divide;</td>
           </tr>
           <tr>
-            <td>7</td>
+            <td onClick={() => handleInput(7)}>7</td>
             <td>8</td>
             <td>9</td>
             <td>&#215;</td>
@@ -33,7 +54,7 @@ export default function SimpleCalculator() {
             <td>4</td>
             <td>5</td>
             <td>6</td>
-            <td>_</td>
+            <td>-</td>
           </tr>
           <tr>
             <td>1</td>
